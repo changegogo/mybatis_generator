51b04e2ada8f81b8e87be212f7a4561ff0051334
dailliwang <758017188@qq.com>
2018-01-24 11:10:05 +0800

    mybatis 逆向工程工具

---
+++ src/com/feicui/po/TUser.java
@@ -0,0 +1,55 @@
+package com.feicui.po;
+
+import java.util.Date;
+
+public class TUser {
+    private Integer uid;
+
+    private String username;
+
+    private String password;
+
+    private Date time;
+
+    private String detail;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
+    }
+}
